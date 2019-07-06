788fb57082dd901a772037ddf2abceae02a8b358
John Park <jhnprk12@gmail.com>
2019-07-06 01:10:03 -0700

    add datasources

---
+++ server/datasources.local.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  "db": {
+    "name": "db",
+    "connector": "memory"
+  },
+  "MongoDB": {
+    "url": "mongodb://localhost:27017",
+    "name": "MongoDB",
+    "connector": "mongodb"
+  }
+}
+
+// {
+//     "db": {
+//       "name": "db",
+//       "connector": "memory"
+//     },
+//     "MongoDB": {
+//       "host": "",
+//       "port": 0,
+//       "url": "",
+//       "database": "",
+//       "password": "",
+//       "name": "MongoDB",
+//       "user": "",
+//       "useNewUrlParser": true,
+//       "connector": "mongodb"
+//     }
+//   }
